7726c9ba461fbd66fb280886b7a22de4f366d62e
helbing23 <helbinrapheal23@gmai.com>
2019-10-02 22:57:10 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,80 @@
+
+/* GOOGLE SIGNIN */
+function onSignIn(googleUser)
+{
+    var profile=googleUser.getBasicProfile();
+    $(".g-signin2").css("display","none");
+    $(".data").css("display","block");
+    $("#pic").attr("src",profile.getImageUrl());
+    $("#email").text(profile.getEmail());
+}
+
+function signOut(){
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function(){
+        alert("You have been successfully signed out");
+        $(".g-signin2").css("display","block");
+        $(".data").css("display","none");
+        
+    });
+}
+
+/* GOOGLE SIGNIN ENDS*/
+
+
+
+/*CHECKLIST APP*/
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+for (var i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+for (var i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("check").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (var i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+/*CHECKLIST APP ENDS*/
